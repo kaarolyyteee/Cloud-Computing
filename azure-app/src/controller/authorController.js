ffb9cbb127ee2f3b48f1948c2a677ddf584d0f66
kaarolyyteee <karolinagribauskaite@gmail.com>
2022-10-13 21:16:54 +0300

    server side

---
+++ azure-app/src/controller/authorController.js
@@ -0,0 +1,76 @@
+import express from "express";
+import mysql from "mysql2/promise";
+import { DB_CONFIG } from "../config/dbConfig.js";
+
+export const router = express.Router();
+
+// CREATE
+router.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+    const connection = await mysql.createConnection(DB_CONFIG);
+    const [response] = await connection.query("INSERT INTO author SET ?", {
+      name,
+    });
+    await connection.end();
+    return res.json(response);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// READ many
+router.get("/", async (_, res) => {
+  try {
+    const connection = await mysql.createConnection(DB_CONFIG);
+    const [rows] = await connection.query(`SELECT * FROM author`);
+    res.send(rows);
+    await connection.end();
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// READ one
+router.get("/:id", async (req, res) => {
+  try {
+    const connection = await mysql.createConnection(DB_CONFIG);
+    const [rows] = await connection.query(
+      `SELECT * FROM author WHERE id = ${req.params.id}`
+    );
+    res.send(rows[0]);
+    await connection.end();
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// UPDATE
+router.put("/", async (req, res) => {
+  try {
+    const { id, name } = req.body;
+    const connection = await mysql.createConnection(DB_CONFIG);
+    const [response] = await connection.query(
+      `UPDATE author SET name="${name}" WHERE id="${id}"`
+    );
+    await connection.end();
+    return res.json(response);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+// DELETE
+router.delete("/", async (req, res) => {
+  try {
+    const { id } = req.body;
+    const connection = await mysql.createConnection(DB_CONFIG);
+    const [response] = await connection.query("DELETE FROM author WHERE id=?", [
+      id,
+    ]);
+    await connection.end();
+    return res.json(response);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
--- azure-app/src/index.js
+++ azure-app/src/index.js
@@ -1,15 +1,16 @@
+import axios from "axios";
 import express from "express";
-import dotenv from "dotenv";
+import "dotenv/config";
 import path from "path";
 import { fileURLToPath } from "url";
+
+import { router as authorRouter } from "./controller/authorController.js";
+import { router as postRouter } from "./controller/postController.js";
 
 // find path of file
 const __filename = fileURLToPath(import.meta.url);
 // take the directory name
 const __dirname = path.dirname(__filename);
-
-// environment variables
-dotenv.config();
 
 const app = express();
 const port = process.env.PORT;
@@ -18,10 +19,33 @@ const port = process.env.PORT;
 app.set("view engine", "ejs");
 // change path of views
 app.set("views", path.join(__dirname, "/views"));
+// static files. Like css/image
+app.use(express.static(__dirname + "/public"));
+//
+app.use(express.json());
 
+// backend routes
+app.use("/api/v1/author", authorRouter);
+app.use("/api/v1/post", postRouter);
+
+// frontend routes
 // home page
-app.get("/", (_, res) => {
-  res.render("pages/home");
+app.get("/", async (_, res) => {
+  try {
+    const authors = await axios
+      .get("https://blog-webapp.azurewebsites.net/api/v1/author")
+      .then((response) => response.data);
+
+    const posts = await axios
+      .get("https://blog-webapp.azurewebsites.net/api/v1/post")
+      .then((response) => response.data);
+
+    console.log(posts);
+
+    res.render("pages/home", { posts: posts });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 });
 
 // about page
@@ -29,6 +53,11 @@ app.get("/about", (_, res) => {
   res.render("pages/about");
 });
 
+// other pages return not found
+app.all("*", (_, res) => {
+  res.status(404).send("Not Found");
+});
+
 app.listen(port, () => {
-  console.log("Server is running");
+  console.log(`Server is running on ${port}`);
 });
